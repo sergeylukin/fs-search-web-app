c541b33534ecdf8517e3f820437a73130f851f39
Sergey Lukin <contact@sergeylukin.com>
2021-02-26 15:42:13 +0200

    Patch back-end (CORS, 404s, etc.)

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
 const glob = require('glob');
 
 const host = '0.0.0.0'
 const port = 1337
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World from torii-backend!')
@@ -13,10 +16,13 @@ app.get('/search', (req, res) => {
    const searchQuery = req.query._q
    glob('/filesystem/' + searchQuery, {}, (err, files) => {
        if (files.length === 0) {
-         res.status(404).send(`Not found`)
+         res.status(404)
+       } else {
+         files.forEach((val, index) => {
+           files[index] = files[index].replace(`/filesystem`, ``)
+         })
        }
-       files.forEach((val, index) => files[index] = files[index].replace(`/filesystem/`, `/`))
-       res.send(files)
+       res.send({data: files})
    })
 });
 
